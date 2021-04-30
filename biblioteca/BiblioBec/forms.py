561d5f31c086a0a6f6e0b650c9d226894d37c2c8
maruizr <maribelruizr303@gmail.com>
2021-04-30 03:39:59 -0400

    Creación de templates catalogos, creación de vistas para listar libros, cambio del favicon, cambio en urls, creación de catalago.css

--- biblioteca/BiblioBec/forms.py
+++ biblioteca/BiblioBec/forms.py
@@ -15,9 +15,3 @@ class EjemplarForm(forms.ModelForm):
     class Meta:
         model = Ejemplar
         fields = '__all__'
-
-class CategoriaForm(forms.ModelForm):
-
-    class Meta:
-        model = Categoria
-        fields = '__all__'
--- biblioteca/BiblioBec/views.py
+++ biblioteca/BiblioBec/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
-from .forms import CategoriaForm
+from .forms import DocumentoForm , EjemplarForm
+from django.http import HttpResponse
+from .models import Libro
+from django.db import connection
+import cx_Oracle
+import base64
+from django.core.files.base import ContentFile
 # Create your views here.
 
 def index(request):
@@ -10,10 +16,44 @@ def index(request):
     )
 
 def catalogo(request):
-   
+
+    data = { 
+        'libros': lista_doc()
+    }
     return render(
         request,
         'catalogo.html',
+        data
+    )
+
+def catalogo_audio(request):
+    data = { 
+        'audios': listado_audios()
+    }
+    return render(
+        request,
+        'libros/catalogo_audio.html',
+        data
+    )
+
+def catalogo_video(request):
+    data = { 
+        'videos': listado_videos()
+    }
+    return render(
+        request,
+        'libros/catalogo_videos.html',
+        data
+    )
+
+def catalogo_libro(request):
+    data = { 
+        'libro': listado_libro()
+    }
+    return render(
+        request,
+        'libros/catalogo_libros.html',
+        data
     )
 
 def solicitudes(request):
@@ -25,8 +65,121 @@ def solicitudes(request):
 
 # vistas de documentos
 
-def create_doc(request):
+def form_doc(request):
     data = {
-        'form': CategoriaForm
+        'form': DocumentoForm(),
+        "doc": lista_doc()
+
     }
     return render(request, 'documento/create_doc.html', data)
+
+def create_doc(request):
+    #return HttpResponse(request.POST.get('autor',''))
+    #return HttpResponse(request.FILES['imagen'])
+    if request.method == 'POST':
+        isbn = request.POST.get('isbn','')
+        titulo = request.POST.get('titulo','')
+        autor = request.POST.get('autor','')
+        editorial = request.POST.get('editorial','')
+        fecha = request.POST.get('fecha_publicacion','')
+        categoria = request.POST.get('categoria_id_cate','')
+        tipo_doc = request.POST.get('tipo_documento_id_tipo_doc','')
+        tipo_me = request.POST.get('tipo_medio','')
+        edi = request.POST.get('edicion','')
+        imagen = request.FILES['imagen'].read()
+
+        """
+        isbn = HttpResponse(request.POST.get('isbn',''))
+        titulo = HttpResponse(request.POST.get('titulo',''))
+        autor = HttpResponse(request.POST.get('autor',''))
+        editorial = HttpResponse(request.POST.get('editorial',''))
+        fecha = HttpResponse(request.POST.get('fecha_publicacion',''))
+        categoria = HttpResponse(request.POST.get('categoria_id_cate',''))
+        tipo_doc = HttpResponse(request.POST.get('tipo_documento_id_tipo_doc',''))
+        tipo_me = HttpResponse(request.POST.get('tipo_medio',''))
+        edi = HttpResponse(request.POST.get('edicion',''))
+        """
+
+        agregar_documento(isbn,titulo,autor,editorial,fecha,categoria,tipo_doc,tipo_me,edi,imagen)
+        data = {
+            'form': DocumentoForm(),
+            "doc": lista_doc()
+
+        }
+    return render(request,'documento/create_doc.html',data)
+
+
+def agregar_documento(isbn,titulo,autor,editorial,fecha,categoria,tipo_doc,tipo_me,edi,imagen):
+    cursor_dj = connection.cursor()
+    cursor_ex = cursor_dj.connection.cursor() 
+    salida = cursor_ex.var(cx_Oracle.NUMBER)
+    cursor_ex.callproc('P_AGREGAR_DOCUMENTO',[isbn,titulo,autor,editorial,fecha,categoria,tipo_doc,tipo_me,edi,imagen,salida])
+
+
+def lista_doc():
+
+    cursor_dj = connection.cursor()
+    cursor_ex = cursor_dj.connection.cursor() 
+    cursor_out= cursor_dj.connection.cursor() 
+    cursor_ex.callproc('P_LISTA_DOCUMENTOS',[cursor_out])
+
+    documentos = []
+    for i in cursor_out:
+        documentos.append(        {
+            'data':i,
+            'imagen':str(base64.b64encode(i[9].read()),'utf-8')
+        })
+
+    return documentos
+
+# Listado de libros - catalogo
+def listado_audios():
+    django_cursor = connection.cursor()
+    cursor = django_cursor.connection.cursor()
+    out_cur = django_cursor.connection.cursor()
+
+    cursor.callproc("SP_LISTAR_AUDIOS", [out_cur])
+
+    lista = []
+
+    for i in out_cur:
+        lista.append(        {
+            'data':i,
+            'imagen':str(base64.b64encode(i[9].read()),'utf-8')
+        })
+    
+    return lista
+
+def listado_videos():
+    django_cursor = connection.cursor()
+    cursor = django_cursor.connection.cursor()
+    out_cur = django_cursor.connection.cursor()
+
+    cursor.callproc("SP_LISTAR_VIDEOS", [out_cur])
+
+    lista = []
+
+    for i in out_cur:
+        lista.append(        {
+            'data':i,
+            'imagen':str(base64.b64encode(i[9].read()),'utf-8')
+        })
+    
+    return lista
+
+def listado_libro():
+    django_cursor = connection.cursor()
+    cursor = django_cursor.connection.cursor()
+    out_cur = django_cursor.connection.cursor()
+
+    cursor.callproc("SP_LISTAR_LIBRO", [out_cur])
+
+    lista = []
+
+    for i in out_cur:
+        lista.append(        {
+            'data':i,
+            'imagen':str(base64.b64encode(i[9].read()),'utf-8')
+        })
+    
+    return lista

ce59e230d622a4bb38b27b21815d42f317638063
Jesus Orellana <orellana.jesus1994@gmail.com>
2021-05-06 19:14:20 -0400

    configurando procedimiento documento update

--- biblioteca/BiblioBec/static/js/doc.js
+++ biblioteca/BiblioBec/static/js/doc.js
@@ -41,6 +41,7 @@ $(document).ready(function() {
   
   function validarForm()
   {
+    alert("asdasdasdas")
     isbn = $('#id_isbn').val()
     titulo=  $('#id_titulo').val()
     autor=  $('#id_autor').val()
@@ -266,6 +267,29 @@ $(document).ready(function() {
         
         $('#id_tipo_medio').focus()
         toastr.error("Debe ingresar el tipo de medio","ERROR",{
+            "closeButton": true,
+            "debug": false,
+            "newestOnTop": false,
+            "progressBar": true,
+            "positionClass": "toast-bottom-right",
+            "preventDuplicates": false,
+            "onclick": null,
+            "showDuration": "300",
+            "hideDuration": "1000",
+            "timeOut": "5000",
+            "extendedTimeOut": "1000",
+            "showEasing": "swing",
+            "hideEasing": "linear",
+            "showMethod": "fadeIn",
+            "hideMethod": "fadeOut"
+        });
+        return false;
+    }
+    if(imagen =="")
+    {   
+        
+        $('#id_imagen').focus()
+        toastr.error("Debe adjuntar una imagen","ERROR",{
             "closeButton": true,
             "debug": false,
             "newestOnTop": false,
--- biblioteca/BiblioBec/static/js/doc_update.js
+++ biblioteca/BiblioBec/static/js/doc_update.js
@@ -354,4 +354,27 @@ function validarForm()
       });
       return false;
   }
+  if(imagen =="")
+  {   
+      
+      $('#id_imagen').focus()
+      toastr.error("Debe adjuntar una imagen","ERROR",{
+          "closeButton": true,
+          "debug": false,
+          "newestOnTop": false,
+          "progressBar": true,
+          "positionClass": "toast-bottom-right",
+          "preventDuplicates": false,
+          "onclick": null,
+          "showDuration": "300",
+          "hideDuration": "1000",
+          "timeOut": "5000",
+          "extendedTimeOut": "1000",
+          "showEasing": "swing",
+          "hideEasing": "linear",
+          "showMethod": "fadeIn",
+          "hideMethod": "fadeOut"
+      });
+      return false;
+  }
 }
